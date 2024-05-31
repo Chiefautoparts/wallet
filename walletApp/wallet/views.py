209bcab010d6211e13b5ea615b07550863b5c3fd
chiefautoparts <chiefautoparts@outlook.com>
2024-05-31 02:24:48 -0400

    push before changing branches

--- walletApp/wallet/views.py
+++ walletApp/wallet/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Wallet
 
-# Create your views here.
+@login_required
+def dashboard(request):
+	wallet, ccreated = Wallet.objects.get_or_create(user=request.user)
+	return render(request, 'wallet/dashboard.html', {'wallet': wallet})
