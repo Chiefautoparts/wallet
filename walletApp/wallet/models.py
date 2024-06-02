fb45f0284b97ba0f4c818076fffe82cf25e8f88a
chiefautoparts <chiefautoparts@outlook.com>
2024-06-02 02:07:42 -0400

    wallet functions

--- walletApp/wallet/models.py
+++ walletApp/wallet/models.py
@@ -9,3 +9,12 @@ class Wallet(models.Model):
 	def __str__(self):
 		return f"{self.user.username}'s Wallet"
 		
+class Transaction(models.Model):
+	wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+	amount = models.DecimalField(max_digits=16, decimal_places=8)
+	transaction_type = models.CharField(max_length=20)
+	transaction_id = models.CharField(max_length=64)
+	date = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return f"{self.transaction_type} - {self.amount} BTC on {self.date}"
